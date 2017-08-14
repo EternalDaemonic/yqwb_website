1bd71b94c282f8b297e3dcc5e39e3074e4bfd50a
zyl <xwykong@163.com>
2017-08-14 12:05:52 +0800

    新的项目

---
+++ myvue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+  	<div class="header">
+				<div class="container">
+					<div class="header-top">
+						<div class="logo">
+							<a href="index.html">1758 <span>定制专家</span></a>
+						</div>
+						<div class="top-menu">
+							<span class="menu"><img src="images/nav.png" alt=""/> </span>
+							<ul>
+								<li><router-link to="/design">设计神器</router-link></li>
+								<li><router-link to="/album">企业相册</router-link></li>
+							</ul>
+						</div>
+						<div class="clerfix"></div>
+					</div>
+				</div>
+			</div>
+    <router-view></router-view>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
