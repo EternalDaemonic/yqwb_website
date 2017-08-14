721ee0722446c698b4c5b23a0d6e501bd121f3da
zyl <xwykong@163.com>
2017-08-14 14:18:26 +0800

    fisr

--- myvue/src/App.vue
+++ myvue/src/App.vue
@@ -7,7 +7,6 @@
 							<a href="index.html">1758 <span>定制专家</span></a>
 						</div>
 						<div class="top-menu">
-							<span class="menu"><img src="images/nav.png" alt=""/> </span>
 							<ul>
 								<li><router-link to="/design">设计神器</router-link></li>
 								<li><router-link to="/album">企业相册</router-link></li>
@@ -28,13 +27,81 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped type="text/css">
+  *{
+    padding: 0;
+    margin: 0;
+  }
+	body a{
+ 		 transition:0.5s all;
 }
-</style>
+  .header {
+    background-color: #f8f8f8;
+    padding: 1em 0;
+  }
+  .logo {
+    float: left;
+  }
+  .logo a {
+    display: block;
+    font-size: 4em;
+    text-decoration: none;
+    color: #DA6A3C;
+    font-weight: 600;
+  }
+  .logo span {
+    color: #222;
+  }
+  .top-menu{
+    float:right;
+    margin-top:1.5em;
+    margin-right: 10px;
+  }
+  .top-menu ul li{
+    display:inline-block;
+    margin:0.5em 1em;
+  }
+  .top-menu ul li a:hover {
+    color: #DA6A3C;
+  }
+  .top-menu ul li a {
+    font-size: 1.2em;
+    text-decoration: none;
+    text-transform: capitalize;
+    color:#747474;
+  }
+  @media (max-width: 991px) {
+    .header {
+      padding: 0.5em 0;
+    }
+    .logo a {
+      font-size: 3em;
+    }
+  }
+  @media (max-width: 640px) {
+    .logo a {
+      font-size: 2.5em;
+    }
+    .top-menu {
+      margin-top: 1em;
+    }
+  }
+  @media (max-width: 480px) {
+    .logo a {
+      font-size: 2.4em;
+    }
+
+    .top-menu {
+      margin-top: 0.7em;
+    }
+  }
+
+  @media (max-width: 320px){
+    .logo a {
+      font-size: 2em;
+    }
+    span.menu img {
+      width: 75%;
+    }}
+</style>push
+
